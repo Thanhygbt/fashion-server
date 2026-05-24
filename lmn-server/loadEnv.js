a7e4516693039fac9e64a4e4066db11d7f8df483
Thanhygbt <thinh123331@gmail.com>
2026-05-24 12:42:18 +0700

    Delete lmn-server directory

--- lmn-server/loadEnv.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const dotenv = require("dotenv");
-
-dotenv.config({
-    path: [
-        path.resolve(__dirname, ".env"),
-        path.resolve(__dirname, "..", ".env")
-    ]
-});
